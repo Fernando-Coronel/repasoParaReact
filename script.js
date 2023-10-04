d80e5bc829350ef1480cb731410203243ce26147
Fernando-Coronel <71113713+Fernando-Coronel@users.noreply.github.com>
2023-10-04 06:52:42 +0000

    Cierre de proyecto 3 y aprendiendo local storage

--- script.js
+++ script.js
@@ -813,149 +813,246 @@
 // }
 //Fin de proyecto
 
-//Proyecto 3. Filtrar y Buscar en base a diferentes Campos
-//Variables
-const marca = document.querySelector("#marca")
-const year = document.querySelector("#year")
-const minimo = document.querySelector("#minimo")
-const maximo = document.querySelector("#maximo")
-const puertas = document.querySelector("#puertas")
-const transmision = document.querySelector("#transmision")
-const color = document.querySelector("#color")
-const resultado = document.querySelector("#resultado");
 
-const max = new Date().getFullYear();
-const min = max - 13 ;
-
-const datosBusqueda = {
-  marca: '',
-  year: '',
-  minimo: '',
-  maximo: '',
-  puertas: '',
-  transmision: '',
-  color: ''
-}
-
-
-//Eventos
-document.addEventListener('DOMContentLoaded', () => {
-  //Muestra los automoviles al cargar
-  mostrarAutos(autos);
-
-  //Llenar las opciones de años
-  llenarSelect();
+ //Proyecto 3. Filtrar y Buscar en base a diferentes Campos.
+  //Variables
+// const marca = document.querySelector("#marca") 
+//  const year = document.querySelector("#year") 
+//  const minimo = document.querySelector("#minimo") 
+//  const maximo = document.querySelector("#maximo") 
+//  const puertas = document.querySelector("#puertas") 
+//  const transmision = document.querySelector("#transmision") 
+//  const color = document.querySelector("#color") 
+//  const resultado = document.querySelector("#resultado"); 
   
-});
-
-marca.addEventListener('change', e => {
-  datosBusqueda.marca = e.target.value;
-  console.log(datosBusqueda);
-
-  filtrarAuto();
-});
-year.addEventListener('change', e => {
-  datosBusqueda.year = parseInt(e.target.value);
-  console.log(datosBusqueda);
-  filtrarAuto();
-});
-minimo.addEventListener('change', e => {
-  datosBusqueda.minimo = e.target.value;
-  console.log(datosBusqueda);
-  filtrarAuto();
-});
-maximo.addEventListener('change', e => {
-  datosBusqueda.maximo = e.target.value;
-  console.log(datosBusqueda);
-  filtrarAuto();
-});
-puertas.addEventListener('change', e => {
-  datosBusqueda.puertas = e.target.value;
-  console.log(datosBusqueda);
-  filtrarAuto();
-});
-transmision.addEventListener('change', e => {
-  datosBusqueda.transmision = e.target.value;
-  console.log(datosBusqueda);
-  filtrarAuto();
-});
-color.addEventListener('change', e => {
-  datosBusqueda.color = e.target.value;
-  console.log(datosBusqueda);
-  filtrarAuto();
-});
-
-
-//Funciones
-function mostrarAutos(autos){
+//  const max = new Date().getFullYear(); 
+//  const min = max - 13 ; 
   
-  autos.forEach( auto => {
-  const {marca, modelo, year, precio, puertas, color, transmision} = auto;
-  const autosHTML = document.createElement('p');
+//  const datosBusqueda = { 
+//    marca: '', 
+//    year: '', 
+//    minimo: '', 
+//    maximo: '', 
+//    puertas: '', 
+//    transmision: '', 
+//    color: '' 
+//  } 
+  
+  
+//  //Eventos 
+//  document.addEventListener('DOMContentLoaded', () => { 
+//    //Muestra los automoviles al cargar 
+//    mostrarAutos(autos); 
+  
+//    //Llenar las opciones de años 
+//    llenarSelect(); 
+  
+//  }); 
+  
+//  marca.addEventListener('change', e => { 
+//    datosBusqueda.marca = e.target.value; 
+//    console.log(datosBusqueda); 
+  
+//    filtrarAuto(); 
+//  }); 
+//  year.addEventListener('change', e => { 
+//    datosBusqueda.year = parseInt(e.target.value); 
+//    console.log(datosBusqueda); 
+//    filtrarAuto(); 
+//  }); 
+//  minimo.addEventListener('change', e => { 
+//    datosBusqueda.minimo = e.target.value; 
+//    console.log(datosBusqueda);
+//    filtrarAuto();
+   
+//  }); 
+//  maximo.addEventListener('change', e => { 
+//    datosBusqueda.maximo = e.target.value; 
+//    console.log(datosBusqueda); 
+//    filtrarAuto();
+   
+//  }); 
+//  puertas.addEventListener('change', e => { 
+//    datosBusqueda.puertas = parseInt(e.target.value); 
+//    console.log(datosBusqueda); 
+//    filtrarAuto();
+   
+//  }); 
+//  transmision.addEventListener('change', e => { 
+//    datosBusqueda.transmision = e.target.value; 
+//    console.log(datosBusqueda); 
+//    filtrarAuto();
+//  }); 
+//  color.addEventListener('change', e => { 
+//    datosBusqueda.color = e.target.value; 
+//    console.log(datosBusqueda); 
+//    filtrarAuto();
+//  }); 
+  
+  
+//  //Funciones 
+//  function mostrarAutos(autos){ 
+   
+//    limpiarHTML();
+  
+//    autos.forEach( auto => { 
+//    const {marca, modelo, year, precio, puertas, color, transmision} = auto; 
+//    const autosHTML = document.createElement('p'); 
+  
+//    autosHTML.textContent = ` 
+//      ${marca} ${modelo} - ${year} - Precio $${precio} - ${puertas} Puertas - Transmision: ${transmision} - Color ${color}  
+//    `   
+//    resultado.appendChild(autosHTML);   
+//    }); 
+  
+//  } 
+ 
+//  function limpiarHTML(){
+//    while(resultado.firstChild){
+//      resultado.removeChild(resultado.firstElementChild);
+//    }
+//  }
+  
+//  function llenarSelect(){ 
+  
+//    for(let i = max; i >= min; i--){ 
+//      const opcion = document.createElement('option'); 
+//      opcion.value = i; 
+//      opcion.textContent = i; 
+//      year.appendChild(opcion);//Agregar las opciones del año al select 
+//    } 
+//  } 
+  
+//  function filtrarAuto(){ 
+//    const resultado = autos.filter( filtrarMarca ).filter( filtrarYear).filter( filtrarMinimo).filter( filtrarMaximo).filter( filtrarPuertas).filter( filtrarTransmision).filter( filtrarColor); 
+//    console.log(resultado); 
+   
+//    if(resultado.length){
+//      mostrarAutos(resultado); 
+//    }else{
+//      noResultado();
+//    }
+//  } 
+ 
+//  function noResultado(){
+//    limpiarHTML();
+//    const noResultado = document.createElement('div');
+//    noResultado.classList.add('alerta', 'error');
+//    noResultado.textContent = 'No hay resultados, intenta con otros terminos de búsqueda';
+//    resultado.appendChild(noResultado);
+//  }
+  
+//  function filtrarMarca(auto){ 
+//    const {marca} = datosBusqueda; 
+//    if(marca){ 
+//      return auto.marca === marca; 
+//    } 
+  
+//    return auto; 
+//  } 
+  
+//  function filtrarYear(auto){ 
+//    const {year} = datosBusqueda; 
+//    if(year){ 
+//      return auto.year === year; 
+//    } 
+//    return auto; 
+//  }
+ 
+//  function filtrarMinimo(auto){
+//    const {minimo} = datosBusqueda;
+//    if(minimo){
+//      return auto.precio >= minimo;
+//    }
+//    return auto;
+//  }
+ 
+//  function filtrarMaximo(auto){
+//    const {maximo} = datosBusqueda;
+//    if(maximo){
+//      return auto.precio <= maximo;
+//    }
+//    return auto;
+//  }
+ 
+//  function filtrarPuertas(auto){
+//    const {puertas} = datosBusqueda;
+//    if(puertas){
+//      return auto.puertas === puertas;
+//    }
+//    return auto
+//  }
+ 
+//  function filtrarTransmision(auto){
+//    const {transmision} = datosBusqueda;
+//    if(transmision){
+//      return auto.transmision === transmision;
+//    }
+//    return auto;
+//  }
+ 
+//  function filtrarColor(auto){
+//    const {color} = datosBusqueda;
+//    if(color){
+//      return auto.color === color;
+//    }
+//    return auto;
+// } 
+ 
+//Local storage
+//Ingresar datos en el local storage
 
-  autosHTML.textContent = `
-    ${marca} ${modelo} - ${year} - Precio $${precio} - ${puertas} Puertas - Transmision: ${transmision} - Color ${color} 
-  `  
-  resultado.appendChild(autosHTML);  
-  });
+localStorage.setItem("Nombre", "Fernando");
 
+const producto = {
+  nombre: "Monitor 24 pulgadas",
+  precio: 2500
 }
 
-function llenarSelect(){
+const productoString = JSON.stringify(producto);
+localStorage.setItem("Producto", productoString);
 
-  for(let i = max; i >= min; i--){
-    const opcion = document.createElement('option');
-    opcion.value = i;
-    opcion.textContent = i;
-    year.appendChild(opcion);//Agregar las opciones del año al select
-  }
-}
+const nombres = ["Fernando", "Elena", "Fatima", "Leonel"];
 
-function filtrarAuto(){
-  const resultado = autos.filter( filtrarMarca ).filter( filtrarYear);
-  console.log(resultado);
-
-  mostrarAutos(resultado);
-}
-
-function filtrarMarca(auto){
-  const {marca} = datosBusqueda;
-  if(marca){
-    return auto.marca === marca;
-  }
-
-  return auto;
-}
-
-function filtrarYear(auto){
-  const {year} = datosBusqueda;
-  if(year){
-    return auto.year === year;
-  }
-  return auto;
-}
+const nombresString = JSON.stringify(nombres);
+localStorage.setItem("Nombres", nombresString);
 
 
+//Obtener datos del local Storage
 
+const getNombre = localStorage.getItem("Nombre");
+console.log(getNombre);
 
+const getProducto = localStorage.getItem("Producto");
+console.log(getProducto);
 
+const getNombres = localStorage.getItem("Nombres");
+console.log(getNombres);
 
+const convertirNombres = JSON.parse(getNombres);
+console.log(convertirNombres);
 
+const convertirProducto = JSON.parse(getProducto);
+console.log(convertirProducto);
 
+//Eliminar datos de local storage
+localStorage.removeItem("Nombre");
 
+//Actualizar un registro
 
+const nombresJSON = JSON.parse(localStorage.getItem("Nombres"));
+console.log(nombresJSON);
+nombresJSON.push("Nuevo nombre");
+console.log(nombresJSON);
+localStorage.setItem("Nombres", JSON.stringify(nombresJSON));
 
+const productoJSON = JSON.parse(localStorage.getItem("Producto"));
+productoJSON.color = "Negro";
+productoJSON.input = "HDMI";
+localStorage.setItem("Producto", JSON.stringify(productoJSON));
 
-
-
-
-
-
-
-
-
-
-
+//Elimina todo el local storage
+localStorage.clear();
 
 
 
