1d3cc4bc344829487ec575a774231c52cd03cf3c
Fernando-Coronel <71113713+Fernando-Coronel@users.noreply.github.com>
2023-10-10 05:19:33 +0000

    fechas

--- script.js
+++ script.js
@@ -1333,7 +1333,19 @@
 // }
 //Fin del proyecto
 
+//Fechas en javaScript
 
+const diaHoy = new Date();
+let valor;
+
+valor = diaHoy;
+valor = diaHoy.getFullYear();
+valor = diaHoy.getMonth();
+valor = diaHoy.getMinutes();
+valor = diaHoy.getHours();
+valor = diaHoy.getTime();
+
+console.log(valor);
 
 
 
