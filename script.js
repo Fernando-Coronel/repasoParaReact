f236f59798a14506ee7934dcefff9dfb7d64b7c5
Fernando-Coronel <71113713+Fernando-Coronel@users.noreply.github.com>
2023-10-17 03:23:11 +0000

    termino de proyecto 6 y comienzo con POO

--- script.js
+++ script.js
@@ -1455,192 +1455,237 @@
 
 //Proyecto 6. Cotizador con prototypes
 //constructor
-function Seguro(marca, year, tipo){
-  this.marca = marca;
-  this.year = year;
-  this.tipo = tipo;
-}
+// function Seguro(marca, year, tipo){
+//   this.marca = marca;
+//   this.year = year;
+//   this.tipo = tipo;
+// }
 
-//Realiza la cotización con los datos
-Seguro.prototype.cotizarSeguro = function(){
-  /*
-  1.Americano = 1.15
-  2.Asiatico = 1.05
-  3.Europeo = 1.35
-  */
-  console.log(this.marca);
+// //Realiza la cotización con los datos
+// Seguro.prototype.cotizarSeguro = function(){
+//   /*
+//   1.Americano = 1.15
+//   2.Asiatico = 1.05
+//   3.Europeo = 1.35
+//   */
+//   console.log(this.marca);
 
-  let cantidad;
-  const base = 2000;
+//   let cantidad;
+//   const base = 2000;
 
-  switch (this.marca) {
-    case '1':
-      cantidad = base * 1.15;
-      break;
-    case '2':
-      cantidad = base * 1.05
-      break;
-    case '3':
-      cantidad = base * 1.35
-      break;
+//   switch (this.marca) {
+//     case '1':
+//       cantidad = base * 1.15;
+//       break;
+//     case '2':
+//       cantidad = base * 1.05
+//       break;
+//     case '3':
+//       cantidad = base * 1.35
+//       break;
 
-    default:
-      break;
-  }
+//     default:
+//       break;
+//   }
 
-  //Leer el año
-  const diferencia = new Date().getFullYear() - this.year;
+//   //Leer el año
+//   const diferencia = new Date().getFullYear() - this.year;
 
-  //Cada añoque la diferencia es mayor, el costo va a reducirse un 3%
-  cantidad -= ((diferencia * 3) * cantidad) / 100;
+//   //Cada añoque la diferencia es mayor, el costo va a reducirse un 3%
+//   cantidad -= ((diferencia * 3) * cantidad) / 100;
 
-  /*
-    Si el seguro es basico se multiplica por un 30% mas
-    Si el seguro es completo se multiplica por un 50% mas
-  */
+//   /*
+//     Si el seguro es basico se multiplica por un 30% mas
+//     Si el seguro es completo se multiplica por un 50% mas
+//   */
 
-  if(this.tipo === 'basico'){
-    cantidad *= 1.30;
-  }else{
-    cantidad *= 1.50;
-  }
+//   if(this.tipo === 'basico'){
+//     cantidad *= 1.30;
+//   }else{
+//     cantidad *= 1.50;
+//   }
 
-  return Math.round(cantidad);
+//   return Math.round(cantidad);
 
-  console.log(Math.floor(cantidad));
-}
+//   console.log(Math.floor(cantidad));
+// }
 
-function UI(){}
+// function UI(){}
 
-//Llena las opciones de los años
-UI.prototype.llenarOpciones = () => {
-  const max = new Date().getFullYear();
-  const min = max - 23;
-  const selectYear = document.querySelector('#year');
+// //Llena las opciones de los años
+// UI.prototype.llenarOpciones = () => {
+//   const max = new Date().getFullYear();
+//   const min = max - 23;
+//   const selectYear = document.querySelector('#year');
 
-  for(let i = max; i > min; i--){
-    let option = document.createElement('option');
-    option.value = i;
-    option.textContent = i;
-    selectYear.appendChild(option);
-  }
-}
+//   for(let i = max; i > min; i--){
+//     let option = document.createElement('option');
+//     option.value = i;
+//     option.textContent = i;
+//     selectYear.appendChild(option);
+//   }
+// }
 
-UI.prototype.mostrarMensaje = (mensaje,tipo) => {
-  const div = document.createElement('div');
+// UI.prototype.mostrarMensaje = (mensaje,tipo) => {
+//   const div = document.createElement('div');
 
-  if(tipo === 'error'){
-    div.classList.add('error');
-  }else{
-    div.classList.add('correcto');
-  }
+//   if(tipo === 'error'){
+//     div.classList.add('error');
+//   }else{
+//     div.classList.add('correcto');
+//   }
 
-  div.classList.add('mensaje', 'mt-10');
-  div.innerText = mensaje;
+//   div.classList.add('mensaje', 'mt-10');
+//   div.innerText = mensaje;
 
-  //Insertar en el html
-  const formulario = document.querySelector('#cotizar-seguro');
-  formulario.insertBefore(div, document.querySelector('#resultado'));
+//   //Insertar en el html
+//   const formulario = document.querySelector('#cotizar-seguro');
+//   formulario.insertBefore(div, document.querySelector('#resultado'));
 
-  setTimeout(()=>{
-    div.remove();
-  },3000)
-}
+//   setTimeout(()=>{
+//     div.remove();
+//   },3000)
+// }
 
-UI.prototype.mostrarResultado = (total, seguro) => {
-  //Crear resultado
-  const {marca, year, tipo} = seguro;
+// UI.prototype.mostrarResultado = (total, seguro) => {
+//   //Crear resultado
+//   const {marca, year, tipo} = seguro;
 
-  let textoMarca;
+//   let textoMarca;
 
-  switch(marca){
-    case '1':
-      textoMarca = 'Americano';
-      break;
-    case '2':
-      textoMarca = 'Asiatico';
-      break;
-    case '3':
-      textoMarca = 'Europeo';
-    default:
-      break;
-  }
+//   switch(marca){
+//     case '1':
+//       textoMarca = 'Americano';
+//       break;
+//     case '2':
+//       textoMarca = 'Asiatico';
+//       break;
+//     case '3':
+//       textoMarca = 'Europeo';
+//     default:
+//       break;
+//   }
   
-  const div = document.createElement('div');
-  div.classList.add('mt-10');
+//   const div = document.createElement('div');
+//   div.classList.add('mt-10');
 
-  div.innerHTML = `
-    <p class="header">Tu resumen</p>
-    <p class="font-bold">Marca: <span class="font-normal">${textoMarca}</span></p>
-    <p class="font-bold">Año: <span class="font-normal">${year}</span></p>
-    <p class="font-bold">Tipo de seguro: <span class="font-normal capitalize">${tipo}</span></p>
-    <p class="font-bold">Total:$<span class="font-normal">${total}</span></p>
-  `;
+//   div.innerHTML = `
+//     <p class="header">Tu resumen</p>
+//     <p class="font-bold">Marca: <span class="font-normal">${textoMarca}</span></p>
+//     <p class="font-bold">Año: <span class="font-normal">${year}</span></p>
+//     <p class="font-bold">Tipo de seguro: <span class="font-normal capitalize">${tipo}</span></p>
+//     <p class="font-bold">Total:$<span class="font-normal">${total}</span></p>
+//   `;
 
-  const resultadoDiv = document.querySelector('#resultado');
+//   const resultadoDiv = document.querySelector('#resultado');
 
-  const spinner = document.querySelector('#cargando');
-  spinner.style.display = 'block';
+//   const spinner = document.querySelector('#cargando');
+//   spinner.style.display = 'block';
 
-  setTimeout(() =>{
-    spinner.style.display = 'none';//Se remueve el spinner
-    resultadoDiv.appendChild(div);//Se mostrara el resultado cuando se quite el spinner
-  },3000)
-}
+//   setTimeout(() =>{
+//     spinner.style.display = 'none';//Se remueve el spinner
+//     resultadoDiv.appendChild(div);//Se mostrara el resultado cuando se quite el spinner
+//   },3000)
+// }
 
-const ui = new UI();
-console.log(ui);
+// const ui = new UI();
+// console.log(ui);
 
-document.addEventListener('DOMContentLoaded', () => {
-  ui.llenarOpciones(); //Llena el select con los años...
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//   ui.llenarOpciones(); //Llena el select con los años...
+// })
 
-eventListeners();
-function eventListeners(){
-  const formulario = document.querySelector('#cotizar-seguro');
-  formulario.addEventListener('submit', cotizarSeguro);
-}
+// eventListeners();
+// function eventListeners(){
+//   const formulario = document.querySelector('#cotizar-seguro');
+//   formulario.addEventListener('submit', cotizarSeguro);
+// }
 
-function cotizarSeguro(e){
-  e.preventDefault();
+// function cotizarSeguro(e){
+//   e.preventDefault();
 
-  //Leer la marca seleccionada
-  const marca = document.querySelector('#marca').value;
+//   //Leer la marca seleccionada
+//   const marca = document.querySelector('#marca').value;
 
-  //Leer el año seleccionado
-  const year = document.querySelector('#year').value;
+//   //Leer el año seleccionado
+//   const year = document.querySelector('#year').value;
 
-  //Leer el tipo seleccionado
-  const tipo = document.querySelector('input[name="tipo"]:checked').value;
-  console.log(tipo);
+//   //Leer el tipo seleccionado
+//   const tipo = document.querySelector('input[name="tipo"]:checked').value;
+//   console.log(tipo);
 
-  if(marca === '' || year === '' || tipo === ''){
-   // console.log('No paso la prueba');
-   ui.mostrarMensaje('Todos los campos son obligatorios', 'error');
-   return;
-  }
-  ui.mostrarMensaje('Cotizando...', 'exito');
+//   if(marca === '' || year === '' || tipo === ''){
+//    // console.log('No paso la prueba');
+//    ui.mostrarMensaje('Todos los campos son obligatorios', 'error');
+//    return;
+//   }
+//   ui.mostrarMensaje('Cotizando...', 'exito');
 
-  //Ocultar las cotizaciones previas
-  const resultados = document.querySelector('#resultado div');
-  if(resultados != null){
-    resultados.remove();
-  } 
+//   //Ocultar las cotizaciones previas
+//   const resultados = document.querySelector('#resultado div');
+//   if(resultados != null){
+//     resultados.remove();
+//   } 
 
-  //Instanciar el seguro
-  const seguro = new Seguro(marca, year, tipo);
-  const total = seguro.cotizarSeguro();
-  // console.log(seguro);
-  // seguro.cotizarSeguro();
+//   //Instanciar el seguro
+//   const seguro = new Seguro(marca, year, tipo);
+//   const total = seguro.cotizarSeguro();
+//   // console.log(seguro);
+//   // seguro.cotizarSeguro();
 
-  //Utilizar el prototype que va a cotizar
-  ui.mostrarResultado(total, seguro);
+//   //Utilizar el prototype que va a cotizar
+//   ui.mostrarResultado(total, seguro);
   
-}
+// }
 
 //Fin de proyecto
 
+//Programación orientada a objetos
 
+//Primera forma de declarar e instanciar una clase
+class Cliente1 {
+  constructor(nombre, saldo){
+    this.nombre = nombre;
+    this.saldo = saldo;
+  }
+  //Forma de crear metodos
+  mostrarInformacion(){
+    return `Cliente: ${this.nombre}, su saldo es de $${this.saldo}.`
+  }
+}
+
+const fernando = new Cliente1('Fernando', 400); 
+console.log(fernando);
+console.log(fernando.mostrarInformacion());
+
+//Segunda forma de declarar e instanciar una clase
+const Cliente2 = class{
+  constructor(nombre, saldo){
+    this.nombre =  nombre;
+    this.saldo = saldo;
+  }
+  //Forma de crear metodos
+  mostrarInformacion(){
+    return `Cliente: ${this.nombre}, su saldo es de $${this.saldo}.`
+  }
+}
+
+const fernando2 = new Cliente2('Fernando', 500)
+console.log(fernando2);
+console.log(fernando2.mostrarInformacion());
+
+//Herencia
+
+class Empresa extends Cliente1 {
+  constructor(nombre, saldo, telefono, categoria){
+    super(nombre, saldo)
+    this.telefono = telefono;
+    this.categoria = categoria;
+  }
+}
+
+const empresa1 = new Empresa('Programador', 1200, 5566778899, 'Programacion');
+console.log(empresa1);
 
 
 
